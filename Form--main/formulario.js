242bfb82e693d0ffa4f970cfff1bdf5ddc3acd19
R-Michell <143142905+R-Michell@users.noreply.github.com>
2023-10-11 10:52:30 -0400

    Add files via upload

---
+++ Form--main/formulario.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('emailInput');
+    const passwordInput = document.getElementById('passwordInput');
+    const rememberCheckbox = document.getElementById('rememberCheckbox');
+    const loginBtn = document.getElementById('loginBtn');
+    const userContainerLink = document.getElementById('userContainerLink');
+  
+    
+  
+  });
